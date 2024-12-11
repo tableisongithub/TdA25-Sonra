910f738afb11d3e0fc72a229e0be3396533226a7
table <rafael@kubicek.ai>
2024-12-11 18:50:47 +0100

    Hid the db shit

--- app/app.py
+++ app/app.py
@@ -10,10 +10,10 @@ app = Flask(__name__)
 # )
 
 # ensure the instance folder exists
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
+# try:
+#     os.makedirs(app.instance_path)
+# except OSError:
+#     pass
 
 # db.init_app(app)
 
